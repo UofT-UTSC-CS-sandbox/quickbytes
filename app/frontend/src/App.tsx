001d03375afa9873a1c4c7a83259c0fc2c724d01
John Guirgis <johnguirgis@johns-air.home>
2024-05-30 21:05:07 -0400

    Create README and initial app setup

---
+++ app/frontend/src/App.tsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const handleClick = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/'); // Assuming your server is running on the same host
+      if (response.ok) {
+        const data = await response.json();
+        console.log('Server response:', data);
+        alert(data.data);
+      } else {
+        throw new Error('Failed to fetch data');
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => {
+          setCount((count) => count + 1)
+          handleClick();
+        }}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
