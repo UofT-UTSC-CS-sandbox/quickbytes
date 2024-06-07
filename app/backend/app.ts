38682ade95cd65a3bf86487c46c02ce5687b224d
arcanstone <allan.henriques@mail.utoronto.ca>
2024-06-06 22:18:52 -0400

    feat: Added user registration and login endpoints with firebase auth"
    Still need to bugtest and properly validate email addresses.

--- app/backend/app.ts
+++ app/backend/app.ts
@@ -4,6 +4,11 @@ import { getAnalytics } from "firebase/analytics";
 import { getDatabase, ref, set, get, child } from "firebase/database";
 import express from 'express';
 
+
+import { getAuth, createUserWithEmailAndPassword, sendEmailVerification } from "firebase/auth";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+
 import * as dotenv from 'dotenv';
 dotenv.config();
 // TODO: Add SDKs for Firebase products that you want to use
@@ -30,6 +35,9 @@ const database = getDatabase(fb);
 const app = express();
 const port = 3000;
 
+
+app.use(express.json());
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -37,6 +45,51 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
 });
+
+//Endpoint for account creation
+app.post('/create-account', async (req, res) => {
+  const { email, password } = req.body;
+
+  //Will be changed later, need to check valid email
+  if (!email.endsWith('@utoronto.ca')) {
+    return res.status(400).send('invalid email');
+  }
+
+  const auth = getAuth(fb);
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    await sendEmailVerification(userCredential.user);
+    res.status(200).send('email sent');
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).send(error.message);
+    } else {
+      res.status(500).send('unknown error');
+    }
+  }
+});
+
+
+//Endpoint for login
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  const auth = getAuth(fb);
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    if (!userCredential.user.emailVerified) {
+      return res.status(400).send('email not verified');
+    }
+    res.status(200).send('successful');
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(401).send(error.message);
+    } else {
+      res.status(401).send('unknown error');
+    }
+  }
+});
+
 
 // Define a GET endpoint
 app.get('/', (req, res) => {
