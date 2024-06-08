274b109bc92bbc31fe009b89e19437f5e7ce5df6
Harris Chong <harrisbchong@gmail.com>
2024-06-07 21:17:31 -0400

    feat: Multi-page routing setup for Settings and Tracking, Navbar component, and styling

--- app/frontend/src/components/DirectionsMap.tsx
+++ app/frontend/src/components/DirectionsMap.tsx
@@ -1,61 +1,66 @@
-import {APIProvider, Map, MapCameraChangedEvent, useMap, useMapsLibrary} from '@vis.gl/react-google-maps';
-import { useState, useEffect } from 'react'
-export default function DirectionsMap () {
-    const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
-    return (
-        <APIProvider apiKey={apiKey} onLoad={() => console.log('Maps API has loaded.')}>
-            <Map
-                defaultZoom={13}
-                defaultCenter={ { lat: -33.860664, lng: 151.208138 } }
-                onCameraChanged={ (ev: MapCameraChangedEvent) =>
-                console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
-                }>
-            <Directions />
-            </Map>
-        </APIProvider>
+import { APIProvider, Map, MapCameraChangedEvent, useMap, useMapsLibrary } from '@vis.gl/react-google-maps';
+import { useState, useEffect } from 'react';
+import './DirectionsMap.css';
+
+export default function DirectionsMap() {
+  const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+  return (
+    <APIProvider apiKey={apiKey} onLoad={() => console.log('Maps API has loaded.')}>
+      <div className='map-container'>
+        <Directions />
+        <Map
+          id={'map'}
+          defaultZoom={13}
+          defaultCenter={{ lat: -33.860664, lng: 151.208138 }}
+          onCameraChanged={(ev: MapCameraChangedEvent) =>
+            console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
+          }>
+        </Map>
+      </div>
+    </APIProvider>
   );
 }
+
 /* Generates and renders directions */
 function Directions() {
-    const map = useMap();
-    const routesLibrary = useMapsLibrary("routes");
-    const [directionsService, setDirectionsService] = useState<google.maps.DirectionsService>();
-    const [directionsRenderer, setDirectionsRenderer] = useState<google.maps.DirectionsRenderer>();
-    const [routes, setRoutes] = useState<google.maps.DirectionsRoute[]>([]);
-    const [routeIndex, setRouteIndex] = useState(0);
-  
-    const selected = routes[routeIndex];
-    const leg = selected?.legs[0];
-  
-    useEffect(() => {
-      if (!routesLibrary || !map) return;
-      setDirectionsService(new routesLibrary.DirectionsService());
-      setDirectionsRenderer(new routesLibrary.DirectionsRenderer({ map }));
-    }, [routesLibrary, map]);
-  
-    useEffect(() => {
-      if (!directionsRenderer || !directionsService) return;
-      directionsService.route({
-        origin: "1265 Military Trail, Scarborough, ON M1C 1A4",
-        destination: "755 Morningside Ave, Scarborough, ON M1C 4Z4",
-        travelMode: google.maps.TravelMode.WALKING,
-        provideRouteAlternatives: true
-      }).then(result => {
-        directionsRenderer.setDirections(result);
-        setRoutes(result.routes);
-      })
-    }, [directionsService, directionsRenderer]);
-  
-    useEffect(() => {
-      if (!directionsRenderer) return;
-  
-      directionsRenderer.setRouteIndex(routeIndex);
-    }, [routeIndex, directionsRenderer])
-    console.log(directionsService);
-    console.log(routes);
-    if (!leg) return null;
-    console.log(selected)
-    return <div className='directions'>
+  const map = useMap("map");
+  const routesLibrary = useMapsLibrary("routes");
+  const [directionsService, setDirectionsService] = useState<google.maps.DirectionsService>();
+  const [directionsRenderer, setDirectionsRenderer] = useState<google.maps.DirectionsRenderer>();
+  const [routes, setRoutes] = useState<google.maps.DirectionsRoute[]>([]);
+  const [routeIndex, setRouteIndex] = useState(0);
+
+  const selected = routes[routeIndex];
+  const leg = selected?.legs[0];
+
+  useEffect(() => {
+    if (!routesLibrary || !map) return;
+    setDirectionsService(new routesLibrary.DirectionsService());
+    setDirectionsRenderer(new routesLibrary.DirectionsRenderer({ map }));
+  }, [routesLibrary, map]);
+
+  useEffect(() => {
+    if (!directionsRenderer || !directionsService) return;
+    directionsService.route({
+      origin: "1265 Military Trail, Scarborough, ON M1C 1A4",
+      destination: "755 Morningside Ave, Scarborough, ON M1C 4Z4",
+      travelMode: google.maps.TravelMode.WALKING,
+      provideRouteAlternatives: true
+    }).then(result => {
+      directionsRenderer.setDirections(result);
+      setRoutes(result.routes);
+    });
+  }, [directionsService, directionsRenderer]);
+
+  useEffect(() => {
+    if (!directionsRenderer) return;
+    directionsRenderer.setRouteIndex(routeIndex);
+  }, [routeIndex, directionsRenderer]);
+
+  if (!leg) return null;
+
+  return (
+    <div>
       <h2>{selected.summary}</h2>
       <p>{leg.start_address.split(',')[0]} to {leg.end_address.split(',')[0]}</p>
       <p>Distance {leg.distance?.text}</p>
@@ -63,11 +68,13 @@ function Directions() {
       <h2>Other Routes</h2>
       <ul>
         {routes.map((route, index) => {
-          return <li key={index}>
-            <button onClick={() => setRouteIndex(index)}>{route.summary}</button>
-          </li>
+          return (
+            <li key={index}>
+              <button onClick={() => setRouteIndex(index)}>{route.summary}</button>
+            </li>
+          );
         })}
       </ul>
     </div>
-  }
-  
+  );
+}
--- app/frontend/src/main.tsx
+++ app/frontend/src/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './pages/App.tsx'
+import OrderTracking from './pages/OrderTracking.tsx';
+import Settings from './pages/Settings.tsx';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/tracking" element={<OrderTracking />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
 )
