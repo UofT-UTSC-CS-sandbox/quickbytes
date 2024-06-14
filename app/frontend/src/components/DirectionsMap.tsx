17c44edb535c8d7348e32b48cf9c783c3e651eeb
John Guirgis <johnguirgis@Johns-MacBook-Air.local>
2024-06-14 12:14:09 -0400

    feat: add loading screen while fetching location
    Refs: #23

--- app/frontend/src/components/DirectionsMap.tsx
+++ app/frontend/src/components/DirectionsMap.tsx
@@ -6,6 +6,10 @@ import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import './DirectionsMap.css';
 import { getCourierActiveOrder, getOrder } from '../middleware';
+import CircularProgress from '@mui/material/CircularProgress';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 import { Snackbar, Alert } from '@mui/material';
 
 interface DirectionsRoute {
@@ -69,10 +73,11 @@ const responseHandler = (response: Response) => response.json();
 
 interface DirectionProps {
   errorHandler: (error: Error) => void;
+  loadHandler: (loadVal:boolean) => void;
 }
 
 /* Generates and renders directions */
-function Directions({ errorHandler }:DirectionProps) {
+function Directions({ errorHandler, loadHandler }:DirectionProps) {
   const map = useMap("map");
   const routesLibrary = useMapsLibrary("routes");
   const [directionsService, setDirectionsService] = useState<google.maps.DirectionsService>();
@@ -85,20 +90,6 @@ function Directions({ errorHandler }:DirectionProps) {
   const selected = routes[routeIndex];
   const leg = selected?.legs[0];
   
-  function handleLocationError(
-    browserHasGeolocation: boolean,
-    infoWindow: google.maps.InfoWindow,
-    pos: google.maps.LatLng
-  ) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(
-      browserHasGeolocation
-        ? "Error: The Geolocation service failed."
-        : "Error: Your browser doesn't support geolocation."
-    );
-    infoWindow.open(map);
-  }
-
   useEffect(() => {
 
     if (navigator.geolocation) {
@@ -117,11 +108,13 @@ function Directions({ errorHandler }:DirectionProps) {
         () => {
           console.error('loc')
           errorHandler(new Error("Could not find your location, please try again later"));
+          loadHandler(false);
         }
       );
     } else {
       // Browser doesn't support Geolocation
       errorHandler(new Error("Browser doesn't support Geolocation"));
+      loadHandler(false);
     }
 
     getCourierActiveOrder(8).then(responseHandler)
@@ -166,6 +159,7 @@ function Directions({ errorHandler }:DirectionProps) {
 
   if (!leg) return null;
 
+  loadHandler(false);
   /* Render header, status, and buttons based on input parameters, update header and buttons workflow*/
   return (
     <div className='sidebar-container'>
@@ -192,31 +186,43 @@ export default function DirectionsMap() {
     console.error(err);
     setDisplayError(err);
   }
+  const [loading, setLoading] = useState(true);
+  const loadHandler = (loadVal:boolean) => setLoading(loadVal);
   return (
-    <div style={{width: "100%", height: "100vh"}}>
-    <APIProvider apiKey={apiKey} onLoad={() => console.log('Maps API has loaded.')}>
-      <div className='map-container'>
-        <Directions errorHandler={errorHandler} />
-        <Map
-          id={'map'}
-          defaultZoom={13}
-          defaultCenter={{ lat: -33.860664, lng: 151.208138 }}
-          onCameraChanged={(ev: MapCameraChangedEvent) =>
-            console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
-          }>
-        </Map>
-      </div>
-    </APIProvider>
-    <Snackbar open={displayError != null} autoHideDuration={6000} onClose={() => setDisplayError(null)}>
-          <Alert
-            onClose={() => setDisplayError(null)}
-            severity="error"
-            variant="filled"
-            sx={{ width: '100%' }}
-          >
-            {displayError?.message}
-          </Alert>
-    </Snackbar>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: "100%", height: "100vh" }}>
+          <div style={{width: "100%", height: "100vh"}}>
+            <APIProvider apiKey={apiKey} onLoad={() => console.log('Maps API has loaded.')}>
+              <div className='map-container'>
+                <Directions loadHandler={loadHandler} errorHandler={errorHandler} />
+                <Map
+                  id={'map'}
+                  defaultZoom={13}
+                  defaultCenter={{ lat: -33.860664, lng: 151.208138 }}
+                  onCameraChanged={(ev: MapCameraChangedEvent) =>
+                    console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
+                  }>
+                </Map>
+              </div>
+            </APIProvider>
+            <Snackbar open={displayError != null} autoHideDuration={6000} onClose={() => setDisplayError(null)}>
+              <Alert
+                onClose={() => setDisplayError(null)}
+                severity="error"
+                variant="filled"
+                sx={{ width: '100%' }}
+              >
+                {displayError?.message}
+              </Alert>
+            </Snackbar>
+            {loading?
+             <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: 'rgba(0, 0, 0, 0.5)', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+              <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <Typography variant="h6">Loading...</Typography>
+                <CircularProgress />
+              </div>
+            </div> : null}
+
+          </div> 
     </div>
   );
 }
