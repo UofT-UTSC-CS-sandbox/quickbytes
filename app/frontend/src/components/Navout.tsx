b3fa3759cc839bde88906dcaf0b388545c934a9c
arcanstone <allan.henriques@mail.utoronto.ca>
2024-06-16 19:34:38 -0400

    feat: Navbar signin/signout button is based on login state
    This way the user cannot see the sign in button when they are already signed in if they navigate to the root page.

--- app/frontend/src/components/Navout.tsx
+++ app/frontend/src/components/Navout.tsx
@@ -1,31 +1,34 @@
-/*Same as the navbar except it has a logout button and it also has a handler for signing out with firebase
-*/
-
 import './Nav2.css';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../AuthContext'; // Import AuthContext for Firebase authentication
 
 export default function Navout() {
-  const { logout } = useAuth(); //useAuth provides a logout function
+  const { currentUser, logout } = useAuth(); // useAuth provides currentUser and logout function
 
   const handleSignOut = async () => {
     try {
       await logout(); // Calls the logout function from useAuth (Firebase signOut method)
     } catch (error) {
       console.error('Error signing out:', error);
-      // Handle sign-out error if needed
     }
   };
 
   return (
     <nav className="navbar">
       <div className="navbar-title">
-                <Link to="/" style={{ color: 'white' }}>QuickBytes</Link>
+        <Link to="/" style={{ color: 'white' }}>QuickBytes</Link>
       </div>
       <div className="navbar-options">
-        <button className="navbar-signout" onClick={handleSignOut}>Sign out</button>
+        {currentUser ? ( // If currentUser exists ( signed in)
+          <>
+            <button className="navbar-signout" onClick={handleSignOut}>Sign out</button>
+          </>
+        ) : ( // If currentUser does not exist (not signed in)
+          <>
+            <Link to="/login" className="navbar-signout">Sign in</Link>
+          </>
+        )}
       </div>
     </nav>
   );
 }
-
--- app/frontend/src/pages/Firstpage.tsx
+++ app/frontend/src/pages/Firstpage.tsx
@@ -1,12 +1,12 @@
 // src/pages/App.tsx
 import React from 'react';
 import './App.css';
-import NavBar2 from '../components/Navbar2';
+import Navout from '../components/Navout';
 
 const Firstpage: React.FC = () => {
   return (
     <>
-      <NavBar2 /> {/* Add Navbar component */}
+      <Navout /> {/* Add Navbar component */}
       <div id="main-content" style={{ paddingTop: '10vh', backgroundColor: 'white' }}>
             hello
         </div>
