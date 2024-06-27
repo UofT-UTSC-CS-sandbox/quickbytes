4a8dbe5bef40f1b8f52618c31cf0f903cb77ae46
Cheng (William) Huang <49571870+clwillhuang@users.noreply.github.com>
2024-06-27 10:48:23 -0400

    docs: fix jsdoc typo

--- app/frontend/src/services/restaurantService.ts
+++ app/frontend/src/services/restaurantService.ts
@@ -21,7 +21,6 @@ export default {
     /**
      * Get the basic details of a restaurant including the entire menu.
      * @param restaurantId The ID of the restaurant to get the menu for.
-     * @param currentUser The current firebase user that is signed in.
      * @returns Service endpoint to get restaurant information and its menu.
      */
     getMenu: (restaurantId: string | undefined) => 
