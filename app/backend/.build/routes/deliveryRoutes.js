7420d2cb8d5349aa0515d30c275011c8322b0c30
Khushi Desai <khushi.desai@advantawitty.com>
2024-06-29 14:40:24 -0400

    fix: deleted extra files

--- app/backend/.build/routes/deliveryRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const deliveryController_1 = require("../controllers/deliveryController");
-const deliveryRouter = (0, express_1.Router)();
-deliveryRouter.get('/active', deliveryController_1.getActiveDelivery);
-deliveryRouter.get('/ordering', deliveryController_1.getAvailableDeliveries);
-deliveryRouter.post('/accept', deliveryController_1.acceptDelivery);
-exports.default = deliveryRouter;
